f1cbbd0cd355fcf8de659ad5456b30d79cd3c4db
Neha Sharma <nehasharma.c2021@gmail.com>
2023-10-24 22:11:43 -0400

    Testing upload file

--- 461Phase2/src/views/UploadView.vue
+++ 461Phase2/src/views/UploadView.vue
@@ -7,6 +7,7 @@
 
 <script>
     import { ref} from "vue"
+    import axios from "axios"
 
     export default{
         name:'Add',
@@ -18,6 +19,19 @@
             // debugger;
                 console.log("selected file",file.value.files)
                 //Upload to server
+                const selectedFile = file.value.files[0]
+                const formData = new FormData();
+                formData.append("file", selectedFile);
+                try {
+                    const response = await axios.post("http://localhost:3000/upload", formData, {
+                        headers: {
+                            "Content-Type": "multipart/form-data",
+                        },
+                    });
+                    console.log("File uploaded successfully.", response.data);
+                } catch (error) {
+                    console.error("Error uploading the file.", error);
+                }
             }
 
             return {
