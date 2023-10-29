da6fa7a6d83c2219e499230accb90125d11c052b
Neha Sharma <nehasharma.c2021@gmail.com>
2023-10-29 05:17:45 -0400

    Implemented download individual, list packages, and search endpoints for RESTful API

--- 461Phase2/app.mjs
+++ 461Phase2/app.mjs
@@ -43,31 +43,90 @@ app.post('/upload', upload.single('file'), (req, res) => {
 });
 
 app.get('/rate/:packageId', (req, res) => {
-    try {
-      const packageId = req.params.packageId;
+  try {
+    const packageId = req.params.packageId;
 
-      //S3 getObject parameters
-      const s3Params = {
-        Bucket: 'your-s3-bucket-name',
-        Key: packageId + '.json', //assuming each rate data is stored as a JSON object
-      };
-      s3.getObject(s3Params, (err, data) => { //request to AWS S3 to retrieve the rate information
-        if (err) {
-          console.error('S3 getObject error:', err);
-          return res.status(500).send('An error occurred while fetching the rate data.');
-        }
-        // Parse the rate data from the response
-        const rateData = JSON.parse(data.Body?.toString() || '');
-        if (!rateData || !rateData.rate) {
-          return res.status(404).send('Rate data not found.');
-        }
-        res.status(200).json(rateData);
-      });
-    } catch (error) {
-      console.error('Error:', error);
-      res.status(500).send('An error occurred.');
+    const s3Params = {
+      Bucket: 'your-s3-bucket-name',
+      Key: packageId + '.json', //assuming each rate data is stored as a JSON object
+    };
+    s3.getObject(s3Params, (err, data) => { 
+      if (err) {
+        console.error('S3 getObject error:', err);
+        return res.status(500).send('An error occurred while fetching the rate data.');
+      }
+      const rateData = JSON.parse(data.Body?.toString() || '');
+      if (!rateData || !rateData.rate) {
+        return res.status(404).send('Rate data not found.');
+      }
+      res.status(200).json(rateData);
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('An error occurred.');
+  }
+});
+
+app.get('/download/:packageId', (req, res) => {
+  try {
+    const packageId = req.params.packageId;
+    const s3Params = {
+      Bucket: 'your-s3-bucket-name',
+      Key: packageId + '.json',
+    };
+    s3.getObject(s3Params)
+      .createReadStream()
+      .pipe(res); //send package directly
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('An error occurred.');
+  }
+});
+
+app.get('/packages', async (req, res) => {
+  try {
+    const s3Params = {
+      Bucket: 'your-s3-bucket-name',
+      Prefix: '', 
+    };
+    const s3Objects = await s3.listObjectsV2(s3Params).promise();
+    const packages = s3Objects.Contents.map((object) => object.Key);
+    //pagination
+    const page = req.query.page || 1;
+    const perPage = req.query.perPage || 10;
+    const startIndex = (page - 1) * perPage;
+    const endIndex = page * perPage;
+    const paginatedPackages = packages.slice(startIndex, endIndex);
+    res.status(200).json(paginatedPackages);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('An error occurred.');
+  }
+});
+
+app.get('/search', async (req, res) => {
+  try {
+    const searchString = req.query.q;
+    if (!searchString) {
+      return res.status(400).send('Search string is required.');
     }
-  });
+    const s3Params = {
+      Bucket: 'your-s3-bucket-name',
+    };
+    const s3Objects = await s3.listObjectsV2(s3Params).promise();
+    const packages = s3Objects.Contents.map((object) => object.Key);
+
+    const searchResults = packages.filter((pkg) => {
+      const regex = new RegExp(searchString, 'i');
+      return regex.test(pkg) || regex.test(pkg + '.readme');
+    });
+
+    res.status(200).json(searchResults);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('An error occurred.');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
